ee525761c14bf04a69a4d776301a3dd056db111a
None <None>
2018-12-09 07:21:24 -0800

    Up try node.py

--- Tests/MaliciousNodesDistributedLAN/stop_python.sh
+++ Tests/MaliciousNodesDistributedLAN/stop_python.sh
@@ -2,10 +2,11 @@
 
 ssh mininet@192.168.0.201 'sudo pkill -9 python'
 ssh mininet@192.168.0.202 'sudo pkill -9 python'
-ssh mininet@192.168.0.203 'sudo pkill -9 python'
 ssh mininet@192.168.0.204 'sudo pkill -9 python'
 ssh mininet@192.168.0.205 'sudo pkill -9 python'
 ssh mininet@192.168.0.206 'sudo pkill -9 python'
+ssh mininet@192.168.0.203 'sudo pkill -9 python'
+
 
 
 
