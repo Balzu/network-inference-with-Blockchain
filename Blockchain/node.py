3497810490f830d654446857344556326d22bb06
None <None>
2018-12-10 13:57:10 -0800

    Fixed problem with socket! Now the system is fully working! :)

--- Blockchain/node.py
+++ Blockchain/node.py
@@ -52,8 +52,39 @@ class server_socket(threading.Thread):
         #print 'Listening on {}:{}'.format(self.ip_addr, self.port)
         self.start_listening(server)
 
+    def recv_timeout(self, the_socket, timeout=2):
+        # make socket non blocking
+        the_socket.setblocking(0)
+        # total data partwise in an array
+        total_data = [];
+        data = '';
+        # beginning time
+        begin = time.time()
+        while 1:
+            # if you got some data, then break after timeout
+            if total_data and time.time() - begin > timeout:
+                break
+            # if you got no data at all, wait a little longer, twice the timeout
+            elif time.time() - begin > timeout * 2:
+                break
+            # recv something
+            try:
+                data = the_socket.recv(8192)
+                if data:
+                    total_data.append(data)
+                    # change the beginning time for measurement
+                    begin = time.time()
+                else:
+                    # sleep for sometime to indicate a gap
+                    time.sleep(0.1)
+            except:
+                pass
+        # join all parts to make final string
+        return ''.join(total_data)
+
     def handle_client_connection(self, client_socket):
-        msg = client_socket.recv(4096) #TODO Era 32768
+        #msg = client_socket.recv(4096) #TODO Era 32768
+        msg = self.recv_timeout(client_socket)
         #self.server.logger().info('\nMsg length: ' + str(len(msg)) + '\n')
         #msg = pickle.loads(msg)
         try:
